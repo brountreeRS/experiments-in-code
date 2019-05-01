007ed28f7bd1e09ef685c908ae99c085bcd7b711
brian rountree <brountree@everquote.com>
2019-05-01 11:55:33 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,110 @@
+({
+    "particles": {
+        "number": {
+            "value": 115,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#fff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 10,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 176.3753266952075,
+            "color": "#ffffff",
+            "opacity": 0.1763753266952075,
+            "width": 1.122388442605866
+        },
+        "move": {
+            "enable": true,
+            "speed": 3.206824121731046,
+            "direction": "bottom-right",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 133.99274002972194,
+                "line_linked": {
+                    "opacity": 0.5
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 6,
+                "duration": 0.3,
+                "opacity": 1,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 56.84540486109416,
+                "duration": 1.0
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
